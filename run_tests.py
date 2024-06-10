b8ed2147b9fa443c4b647d78ea30aedfb77deea4
Vedasitoleh <vedanjali.sitoleh@g.austincc.edu>
2024-06-10 22:46:58 +0000

    Homework2

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
 import unittest
+
 '''
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -5,14 +5,22 @@ The file at /src/homework/b_in_proc_out/output has
 the function get_number.
 '''
 from src.homework.b_in_proc_out.output import get_number
+from src.homework.b_in_proc_out.output import multiply_numbers
 
 class Test_Config(unittest.TestCase):
 
     def test_get_number_1(self):
         #test that the function get_number returns 1
         self.assertEqual(1, get_number(1))
-    
+
     def test_get_number_2(self):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
 
+    def test_multiply_numbers_1(self):
+        #test that 5*5=25
+        self.assertEquals(25, multiply_numbers(5,5), 'WRONG')
+
+    def test_multiply_numbers_2(self):
+        #test that 10*10=100
+        self.assertEquals(100, multiply_numbers(10,10), 'WRONG')
