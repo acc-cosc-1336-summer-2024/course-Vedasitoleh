2da37706bc4e7b0d8d1ea405bce6752e25b82d77
Vedasitoleh <vedanjali.sitoleh@g.austincc.edu>
2024-07-23 21:29:59 +0000

    Homework 3 Vedasitoleh

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,18 @@
-#
+#average rating function
+def get_options_ratio(options, total):
+    rating_num = options/total
+    return rating_num
+
+# rating
+def get_faculty_rating(get_options_ratio):
+    if (get_options_ratio >= 0.9 and get_options_ratio < 1):
+        rating_eval = "Excellent"
+    elif (get_options_ratio >= 0.8 and get_options_ratio < 0.9):
+        rating_eval = "Very Good"
+    elif (get_options_ratio >= 0.7 and get_options_ratio < 0.8):
+        rating_eval = "Good"
+    elif (get_options_ratio >= 0.6 and get_options_ratio < 0.7):
+        rating_eval = "Needs Improvement"
+    elif (get_options_ratio >= 0 and get_options_ratio <= 0.59):
+        rating_eval = "Unacceptable"
+    return rating_eval
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,8 @@
-#
+import decisions
+
+option = float(input("Enter option:"))
+total = float(input("Enter total:"))
+
+ratio = decisions.get_options_ratio(option,total)
+
+print ("Your faculty rating is: "+ decisions.get_faculty_rating(ratio))
