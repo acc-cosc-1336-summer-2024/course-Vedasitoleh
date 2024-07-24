b2d0069fc2be64cb19dca6e646ba48c60e0e0de1
Vedasitoleh <vedanjali.sitoleh@g.austincc.edu>
2024-07-24 02:37:39 +0000

    Veda Sitoleh Homework 4

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,20 @@
-#
+import repetition
+
+user_input = "Y"
+
+while user_input != "3":
+    user_input = input("Select an operation: \n1-Factorial\n2-Sum odd numbers\n3-Exit ")
+    if user_input == "1":
+        num = int(input("Enter a number greater than 0 and less than 100: "))
+        while num <=0 or num >= 100:
+            print("That number is invalid.") 
+            num = int(input("Enter a number greater than 0 and less than 100: "))
+        print("The factorial of "+ str(num) +" is "+ str(repetition.get_factorial(num)))
+       
+    if user_input == "2":
+        num = int(input("Enter a number greater than 0 and less than 100: "))
+        while num <=0 or num >= 100:
+            print("That number is invalid.") 
+            num = int(input("Enter a number greater than 0 and less than 100: "))
+        print("The the sum of the odd numbers that comprise "+ str(num) +" is "+ str(repetition.sum_odd_numbers(num)))
+ 
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,20 @@
-#
+def get_factorial(num):
+    factorial = 1
+    for i in range(1,num + 1):
+       factorial = factorial*i
+    return factorial
+
+def sum_odd_numbers(num):
+    counter = 0
+    odd_sum = 1
+    result = 0
+    if num%2 == 1:
+        odd_counter = (num-1)/2 + 1
+    elif num%2 == 0:
+        odd_counter = num/2
+    while counter < int(odd_counter):
+        counter += 1
+        result += odd_sum
+        odd_sum += 2
+    return result
+
